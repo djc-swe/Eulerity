9a1cffb151bb7f83304800749ba5778c8755c0b2
Daniel Condly <onyxmatrix24@gmail.com>
2024-02-21 09:20:43 -0500

    cocoapods -> 1.15

---
+++ Eulerity/SubclassedTapGestureRecognizer.swift
@@ -0,0 +1,12 @@
+//
+//  SubclassedTapGestureRecognizer.swift
+//  Eulerity
+//
+//  Created by DJC on 12/3/23.
+//
+
+import UIKit
+
+class SubclassedTapGestureRecognizer: UITapGestureRecognizer {
+
+}
